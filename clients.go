a927a470f7f33aebc7bbddc270f274b13edc0445
Kellen Miller <kellen.miller@mercari.com>
2023-02-10 17:04:04 -0500

    update 1007

--- clients.go
+++ clients.go
@@ -11,7 +11,7 @@ import (
 	mercariapiPb "github.com/kouzoh/platform-client-go/src/api-us/v1"
 )
 
-const Version = "v0.1006.0"
+const Version = "v0.1007.0"
 
 type (
 	Services struct {
--- script/update-tag.js
+++ script/update-tag.js
@@ -1,4 +1,10 @@
 module.exports = ({github, context}) => {
 	console.log("hello from js script")
-	console.log(process.env)
+	console.log(process.env.PLATFORM_CLIENT_GO_VERSION)
+	
+	// const execSync = require('child_process').execSync;
+	// // import { execSync } from 'child_process';  // replace ^ if using ES modules
+	//
+	// const output = execSync('ls', { encoding: 'utf-8' });  // the default is 'buffer'
+	// console.log('Output was:\n', output);
 }
