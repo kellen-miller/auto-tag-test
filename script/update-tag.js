1f5a91cf183e75d9eeda38baf997434bb91e9f13
Kellen Miller <kellen.miller@mercari.com>
2023-02-10 18:59:16 -0500

    update 1040

--- script/update-tag.js
+++ script/update-tag.js
@@ -3,11 +3,13 @@ const fs = require('fs')
 module.exports = async function ({github, context}) {
 	const fs = require('fs')
 	
-	const response = await github.rest.repos.listTags({
-		                                                  owner: context.repo.owner,
-		                                                  repo: context.repo.repo,
-		                                                  pattern: 'v[0-9]\+\.[0-9]\+\.[0-9]\+'
-	                                                  })
+	const response = await github.rest.repos.listTags(
+		{
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			pattern: 'v[0-9]\+\.[0-9]\+\.[0-9]\+'
+		}
+	)
 	
 	const file = fs.readFileSync('clients.go', 'utf8')
 	const version = file.match(/v\d+\.\d+\.\d+/)
@@ -20,12 +22,14 @@ module.exports = async function ({github, context}) {
 	})
 	
 	try {
-		github.rest.git.createRef({
-			                          owner: context.repo.owner,
-			                          repo: context.repo.repo,
-			                          ref: `refs/tags/${version}`,
-			                          sha: context.sha
-		                          })
+		github.rest.git.createRef(
+			{
+				owner: context.repo.owner,
+				repo: context.repo.repo,
+				ref: `refs/tags/${version}`,
+				sha: context.sha
+			}
+		)
 	} catch (error) {
 		console.log(error)
 	}
