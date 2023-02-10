a56c8fa4df86900029a848f49b1330a59b35e485
Kellen Miller <kellen.miller@mercari.com>
2023-02-10 17:21:24 -0500

    update 1011

--- script/update-tag.js
+++ script/update-tag.js
@@ -1,12 +1,10 @@
-// import { execSync } from 'child_process';  // replace ^ if using ES modules
-
 module.exports = ({github, context}) => {
-	console.log("hello from js script")
-	console.log(process.env)
-	console.log("GITHUB_ENV", process.env.GITHUB_ENV)
-	console.log("PCGV", process.env.PLATFORM_CLIENT_GO_VERSION)
-	
-	const execSync = require('child_process').execSync;
-	const output = execSync(`cat ${process.env.GITHUB_ENV}`, { encoding: 'utf-8' });
-	console.log('Output was:\n', output);
+	github.rest.git.createRef(
+		{
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			ref: `refs/tags/${process.env.PLATFORM_CLIENT_GO_VERSION}`,
+			sha: context.sha
+		}
+	)
 }
