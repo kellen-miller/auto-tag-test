8b143424e707d7d0ad71ba11f0fd5fc7e4264f44
Kellen Miller <kellen.miller@mercari.com>
2023-02-14 16:20:42 -0500

    fix

--- script/update-latest-tag.js
+++ script/update-latest-tag.js
@@ -7,9 +7,13 @@ module.exports = async function ({github, context}) {
 	console.log("Repo tags: " + JSON.stringify(repoTags))
 
 	let latestTags = getLatestTagsForMajorVersions(repoTags)
-	latestTags = updateMinorVersions(latestTags, majorVersionsUpdated(context))
+	console.log("Latest tags for major: " + JSON.stringify(latestTags))
+	const mvu = majorVersionsUpdated(context)
+	console.log("Major versions updated: " + JSON.stringify(majorVersionsUpdated))
+	latestTags = updateMinorVersions(latestTags, mvu)
+	console.log("Latest tags for major after minor update: " + JSON.stringify(latestTags))
 	latestTags = setPatchVersion(latestTags)
-	
+	console.log("Latest tags for major after patch update: " + JSON.stringify(latestTags))
 	buildTags(latestTags).forEach(tag => createTag(github, context, tag))
 }
 
