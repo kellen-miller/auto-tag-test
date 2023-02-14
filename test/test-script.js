dcbb36758c5f879b529119030b3b23d951fe957f
Kellen Miller <kellen.miller@mercari.com>
2023-02-14 14:03:44 -0500

    change files

--- test/test-script.js
+++ test/test-script.js
@@ -1,3 +1,10 @@
 const script = require('../script/update-latest-tag.js')
+const context = {
+	sha: 'sha',
+	payload: {
+		before: 'before'
+	}
+}
+
 script({  })
 	.then( r => console.log("done", r) )
